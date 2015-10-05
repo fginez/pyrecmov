30347baafcd95fa51d111d96920aadb61c69045a
fginez <fginez@gmail.com>
2015-10-04 21:28:03 -0300

    Amostrador atualizado
    Atualizaçoes de codigo.
    
      fg, 04-10-2015

--- amostrador.py
+++ amostrador.py
@@ -9,7 +9,12 @@ class Amostrador(thread.Thread):
     def __init__(self):
         thread.Thread.__init__(self, self.amostragem)
         self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
+        self.sobreposicao = 0
         self.estado = 0
+        self.pronto = 0
+        self.dt_medio_aq = 0
+        self.f_aq = 0
+        self.t_aq = 0
         pass
 
     def inicia(self):
@@ -23,10 +28,20 @@ class Amostrador(thread.Thread):
         pass
 
     def amostragem(self):
+        janela_baixa = []
+        janela_alta = []
+        n_amostras = 0
+
         while self.estado == 1:
             amostra = []
             resultado = obtem_amostra(self.ser, amostra)
             if resultado == True:
+                janela_alta.append(amostra)
+                n_amostras += 1
+
+                if n_amostras == self.sobreposicao:
+                    pass
+
                 pass
 
     def esta_ativo(self):
