456dd7b6b13bdd0a232063069af17184c3844a3a
fginez <fginez@gmail.com>
2015-10-08 17:51:29 -0300

    Evolução do projeto fg 08-10-2015

---
+++ classificador.py
@@ -0,0 +1,11 @@
+__author__ = 'ginezf'
+from classif import *
+
+
+class Classificador:
+
+    def __init__(self):
+        pass
+
+    def classifica(self, padrao):
+        pass
---
+++ processador.py
@@ -0,0 +1,18 @@
+__author__ = 'ginezf'
+from feature_generation import *
+
+
+class Processador:
+
+    def __init__(self):
+        pass
+
+    def processa(self, janela):
+        x = janela.obtem_eixo("x")
+        y = janela.obtem_eixo("y")
+        z = janela.obtem_eixo("z")
+        vetor = vetor_caracteristicas(x, y, z, len(x))
+        return vetor
+
+    def normaliza(self, padrao):
+        pass
