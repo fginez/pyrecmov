f345bf449b092d6fdec41523473dc25e9b8286c9
fginez <fginez@gmail.com>
2016-03-08 18:11:28 -0300

    Retorna apenas as colunas de dados
    Retorna apenas as colunas que contém as informações de x, y, z e classe.

--- treinamento.py
+++ treinamento.py
@@ -1,8 +1,20 @@
 import scipy as sp
 
+# Formato do arquivo .dat
+# 0 file header (pular)
+# 1 Dados da amostra #1
+# 2 Dados da amostra #2
+# ....
+
+# Formato dos dados da amostra
+#     0           1           2          3    4   5   6   7     8        9
+# Timestamp | Contador | Ticks jitter | spc | ? | X | Y | Z | Classe | vazio
+
 def importa_arquivo(filename):
-    data = sp.genfromtxt(filename, delimiter="|", skip_header=1)
+    data = sp.genfromtxt(filename, delimiter="|", skip_header=1,  usecols=(5, 6, 7, 8))
     return data
 
 filename = "data/sample_database/User1.dat"
-importa_arquivo(filename)
+data = importa_arquivo(filename)
+
+
